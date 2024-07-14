c0148e28d2f90f6955307ed4b3b690e86b089059
ShishirAbylle <nisargamurthybp@gmail.com>
2024-07-14 20:59:38 +0530

    done

---
+++ MyntraProj-main/MyntraProjBack/MyntraProjBack/Models/Item.cs
@@ -0,0 +1,10 @@
+﻿namespace MyntraProjBack.Models
+{
+    public class Item
+    {
+        public int ItemID { get; set; }
+        public string ItemName { get; set; }
+        public string ItemType { get; set; }
+        public string ItemData { get; set; }
+    }
+}
